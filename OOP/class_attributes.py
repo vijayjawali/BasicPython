2813e5841cc472f327c7711fbc5e19f0be8e2c09
vijayjawali <47619951+vijayjawali@users.noreply.github.com>
2024-09-14 23:18:59 +0530

    [Added] OOPS

---
+++ OOP/class_attributes.py
@@ -0,0 +1,9 @@
+class Kettle(object):
+
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+    def switch_on(self):
+        self.on = True
